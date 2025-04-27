3df3b8ed625138b35de9649759cffcc6dd579175
Arunkumargunti-7 <arunkumargunti0123@gmail.com>
2025-04-27 15:31:44 +0530

    Update HeroBanner.tsx

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -32,7 +32,7 @@ const HeroBanner = ({ language }: HeroBannerProps) => {
       {/* Content Overlay */}
       <div className="absolute inset-0 flex items-center justify-center z-20">
         <div className="text-center px-4 py-6 bg-white/70 backdrop-blur-sm rounded-lg w-4/5 mx-auto">
-          <h1 className="text-2xl md:text-xl font-bold text-foreground font-playfair mb-2">
+          <h1 className="text-xl md:text-xl font-bold text-foreground font-playfair mb-2">
             {language === 'en' ? 'Jesus said in reply, "It is written One does not live by bread alone,but by every word that comes forth from the mouth of God." Mathew 4:4' : 'Vislumbre Divino Diario'}
           </h1>
           {/*<p className="text-md md:text-lg text-muted-foreground">
